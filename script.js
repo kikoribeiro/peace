91cbe1d01144f3b97b0f19c93a556c75591c1ca9
kikoribeiro <120732129+kikoribeiro@users.noreply.github.com>
2023-05-24 21:51:26 +0100

    Update script.js sobre api

--- script.js
+++ script.js
@@ -22,4 +22,70 @@ toggleIcon.addEventListener('click', () => {
         toggleIcon.className = 'menuIcon';
     }
 });
+let menuIcon = document.querySelector('.menuIcon');
+let nav = document.querySelector('.overlay-menu');
+
+menuIcon.addEventListener('click', () => {
+    if (nav.style.transform != 'translateX(0%)') {
+        nav.style.transform = 'translateX(0%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    } else { 
+        nav.style.transform = 'translateX(-100%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    }
+});
+
+
+// Toggle Menu Icon ========================================
+let toggleIcon = document.querySelector('.menuIcon');
+
+toggleIcon.addEventListener('click', () => {
+    if (toggleIcon.className != 'menuIcon toggle') {
+        toggleIcon.className += ' toggle';
+    } else {
+        toggleIcon.className = 'menuIcon';
+    }
+});
+//API------------------------------------------------
+
+const TRNApiKey = 'f8fd1723-c779-40ff-ab22-57f475b91fd0';
+
+const fetchCSGOProfile = async (steamId) => {
+  const url = `https://public-api.tracker.gg/v2/csgo/standard/profile/steam/${steamId}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'Accept-Encoding': 'gzip',
+        'TRN-Api-Key': TRNApiKey
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
+
+    const profileData = await response.json();
+    console.log(profileData);
+
+    return profileData;
+  } catch (error) {
+    console.error('Error fetching CSGO profile:', error);
+  }
+};
+const updateProfileInfo = (profileData) => {
+    const profileDiv = document.getElementById('profileData');
+  
+    // Update the HTML with the desired data from the profileData object
+    profileDiv.innerHTML = `
+      <h2>CSGO Profile</h2>
+      <p>Username: ${profileData.data.platformInfo.platformUserHandle}</p>
+      <p>Level: ${profileData.data.segments[0].stats.level.value}</p>
+      <p>Kills: ${profileData.data.segments[0].stats.kills.value}</p>
+      <!-- Add more data as needed -->
+    `;
+  };
+  
+fetchCSGOProfile('76561198008049283');
 
